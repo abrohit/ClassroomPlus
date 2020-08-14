8c8aedff8c345469df2a7faaf764d05a2705ea5f
abrohit <abrohit05@gmail.com>
2020-08-14 23:48:11 +0530

    join session pre-try

--- ClassRoom_Plus/urls.py
+++ ClassRoom_Plus/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
 
     path('newsession/',views.new_session,name='new_session'),
     url(r'^session/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/[1-9][0-9]?$|^100/$',views.user_session, name='user_session'),
+    path('joinsession/',views.joinsession, name='joinsession'),
 
     url(r'^makequiz/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/[1-9][0-9]?$|^100/$',views.makequiz, name='makequiz'),
     url(r'^quizresponse/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/[1-9][0-9]?$|^100/$',views.quizresponse, name='quizresponse'),
--- Home/views.py
+++ Home/views.py
@@ -127,3 +127,7 @@ def icebreaker(request,uidb64, token):
 @login_required
 def freeresponse(request,uidb64, token):
     return(render(request,'home/answerOpenEnded.html'))
+
+@login_required
+def joinsession(request):
+    return(render(request,'home/joinsession.html'))
